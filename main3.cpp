a6c0e858ad4a99c3ae8e9465d28d6767f00fe544
miyasaka <yoyuohlhjl@yahoo.co.jp>
2020-08-15 13:12:19 +0900

    order pi of aig

---
+++ main3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include "aig.hpp"
+
+int main(int argc, char** argv) {
+  if(argc < 3) {
+    std::cerr << "usage : exe <aig> <out.aig>" << std::endl;
+    return 1;
+  }
+  std::string aigname = argv[1];
+  std::string aigname2 = argv[2];
+
+  // read aig
+  aigman aig;
+  aig.read(aigname);
+
+  // order pi
+  std::vector<int> order;
+  for(int i = 0; i < aig.nPis; i++) {
+    order.push_back(i);
+  }
+  for(int i = 0; i < aig.nPis / 2; i++) {
+    {
+      int pos = std::distance(order.begin(), std::find(order.begin(), order.end(), aig.nPis / 2 - i - 1));
+      if(i + i == pos) continue;
+      aig.swappis(i + i, pos);
+      std::swap(order[i + i], order[pos]);
+    }
+    {
+      int pos = std::distance(order.begin(), std::find(order.begin(), order.end(), aig.nPis - i - 1));
+      if(i + i + 1 == pos) continue;
+      aig.swappis(i + i + 1, pos);
+      std::swap(order[i + i + 1], order[pos]);
+    }
+  }
+
+  aig.write(aigname2);
+  
+  return 0;
+}
